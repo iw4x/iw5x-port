d601658a527067c7ef93c1d00081d05467fd461f
Diavolo <edoardo.sanguineti222@gmail.com>
2022-05-22 17:35:52 +0200

    Fix format string for chat glog call
    I based myself off of Black Ops instead of IW4, sry

--- src/module/chat.cpp
+++ src/module/chat.cpp
@@ -22,11 +22,11 @@ static void notify_on_say(game::native::gentity_s* ent, int mode, const char* me
 
 	if (mode == 0)
 	{
-		log_file::g_log_printf("say;%d;%d;%s;%s\n", guid, ent_num, name, message + 1);
+		log_file::g_log_printf("say;%s;%d;%s;%s\n", guid, ent_num, name, message + 1);
 	}
 	else
 	{
-		log_file::g_log_printf("sayteam;%d;%d;%s;%s\n", guid, ent_num, name, message + 1);
+		log_file::g_log_printf("sayteam;%s;%d;%s;%s\n", guid, ent_num, name, message + 1);
 	}
 }
 
